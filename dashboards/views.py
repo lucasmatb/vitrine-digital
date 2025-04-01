57f8ff140846d2f6cb0241a88991e5cfa2e16abc
Lucas Amâncio <lucasmatb2010@hotmail.com>
2025-03-31 21:17:40 -0300

    [develop] - Dashboard usuário pronta, só falta clicável

--- dashboards/views.py
+++ dashboards/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
-from empresas.models import Empresa, Categoria_Empresa
-from usuarios.models import Pedido_Lojista
+from empresas.models import Empresa
+from produtos.models import Produto, Imagem_Produto
 from django.db.models import Count
+from django.db.models.functions import Least
 
 def home(request):
     if request.user.is_authenticated:
@@ -15,13 +16,50 @@ def retorna_dashboard_usuario(request):
     pesquisa = request.GET.get('pesquisa')
 
     if not pesquisa:
-        empresas = Empresa.objects.all().order_by('nome_fantasia').annotate(
+        produtos_um = Produto.objects.annotate(
+            min_date=Least('created_at', 'updated_at'),
+            favoritos=Count('favoritos_produtos')
+        ).filter(preco_oferta__isnull=False).order_by('min_date')
+        data['produtos_um_titulo'] = 'Essas ofertas acabaram de chegar!'
+        data['produtos_um'] = set_favoritos_produto(request, produtos_um)
+
+        empresas_um = Empresa.objects.all().order_by('created_at').annotate(
             favoritos=Count('favoritos_empresas')
         )
+        data['empresas_um_titulo'] = 'Essas empresas acabaram de chegar!'
+        data['empresas_um'] = set_favoritos_empresa(request, empresas_um)
+
+        produtos_dois = Produto.objects.annotate(
+            favoritos=Count('favoritos_produtos', distinct=True)
+        ).order_by('-favoritos', 'nome')
+        data['produtos_dois_titulo'] = 'Os produtos mais curtidos!'
+        data['produtos_dois'] = set_favoritos_produto(request, produtos_dois)
+
+        empresas_dois = Empresa.objects.annotate(
+            favoritos=Count('favoritos_empresas', distinct=True)
+        ).order_by('-favoritos', 'nome_fantasia')
+        data['empresas_dois_titulo'] = ' As empresas mais curtidas!'
+        data['empresas_dois'] = set_favoritos_empresa(request, empresas_dois)
+
+        data['sem_pesquisa'] = True
     else:
-        empresas = Empresa.objects.filter(nome_fantasia__icontains=pesquisa).order_by('nome_fantasia').annotate(
+        empresas_um = Empresa.objects.filter(nome_fantasia__icontains=pesquisa).order_by('nome_fantasia').annotate(
             favoritos=Count('favoritos_empresas')
         )
+        data['empresas_um_titulo']= 'Empresas encontradas'
+        data['empresas_um'] = set_favoritos_empresa(request, empresas_um)
+
+        produtos_um = Produto.objects.filter(nome___icontains=pesquisa).order_by('nome').annotate(
+            favoritos=Count('favoritos_produtos')
+        )
+        data['produtos_um_titulo'] = 'Produtos encontrados'
+        data['produtos_um'] = set_favoritos_produto(request, produtos_um)
+
+        data['sem_pesquisa'] = False
+
+    return render(request, 'dashboard-usuario.html', data)
+
+def set_favoritos_empresa(request, empresas):
 
     empresas_com_favoritos = [
         {
@@ -37,9 +75,29 @@ def retorna_dashboard_usuario(request):
         for empresa in empresas
     ]
 
-    data['empresas'] = empresas_com_favoritos
+    return empresas_com_favoritos
 
-    return render(request, 'dashboard-usuario.html', data)
+def set_favoritos_produto(request, produtos):
+
+    produtos_com_favoritos = [
+        {
+            'id': produto.id,
+            'nome': produto.nome,
+            'descricao': produto.descricao,
+            'preco': produto.preco,
+            'preco_oferta': produto.preco_oferta,
+            'qtd': produto.qtd,
+            'ativo': produto.ativo,
+            'categorias': produto.categoria_produto.all(),
+            'porcentagem_desconto': int(((produto.preco - produto.preco_oferta) / produto.preco) * 100) if produto.preco_oferta is not None else None,
+            'primeira_imagem': Imagem_Produto.objects.filter(id_produto=produto).order_by('id').first().imagem if Imagem_Produto.objects.filter(id_produto=produto).order_by('id').first() else None,
+            'favorito_usuario': produto.favoritos_produtos.filter(id=request.user.id).exists(),
+            'favoritos': produto.favoritos
+        }
+        for produto in produtos
+    ]
+
+    return produtos_com_favoritos
 
 def retorna_dashboard_lojista(request):
 
