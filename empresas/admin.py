b1d116ddec669290bcbc7c7b53078088197dc546
Lucas Amâncio <lucasmatb2010@hotmail.com>
2025-05-25 19:12:44 -0300

    [develop] - Correção permissão admin

--- empresas/admin.py
+++ empresas/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Estado, Cidade, Categoria_Empresa
+from .models import Empresa, Categoria_Empresa
 
-admin.site.register(Estado)
-admin.site.register(Cidade)
 admin.site.register(Categoria_Empresa)
+
+@admin.register(Empresa)
+class ProdutoAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request):
+        return False
--- produtos/admin.py
+++ produtos/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-from .models import Categoria_Produto
+from .models import Produto, Categoria_Produto
 
 admin.site.register(Categoria_Produto)
+
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request):
+        return False
